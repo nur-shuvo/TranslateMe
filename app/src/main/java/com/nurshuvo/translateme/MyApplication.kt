a1545a424291f99467e907055f0206cfeaa4669a
Humayan-Kabir <zyxwv.hk@gmail.com>
2023-02-03 22:34:09 +0600

    added hilt library for dependecy injection

--- app/src/main/java/com/nurshuvo/translateme/MyApplication.kt
+++ app/src/main/java/com/nurshuvo/translateme/MyApplication.kt
@@ -3,7 +3,9 @@ package com.nurshuvo.translateme
 import android.app.Application
 import com.nurshuvo.translateme.data.repository.TranslationRepository
 import com.nurshuvo.translateme.database.TranslationDatabase
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyApplication: Application() {
 
     val translationDatabase: TranslationDatabase by lazy {
