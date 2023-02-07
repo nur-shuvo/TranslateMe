7af10df1230191b9c37e527e02bc7f7b6ab53318
nur-shuvo <nurshuvo51@gmail.com>
2023-02-07 23:55:59 +0600

    Added search view on TranslationListActivity.kt

--- app/src/main/java/com/nurshuvo/translateme/ui/TranslationListActivity.kt
+++ app/src/main/java/com/nurshuvo/translateme/ui/TranslationListActivity.kt
@@ -1,14 +1,19 @@
 package com.nurshuvo.translateme.ui
 
 import android.content.DialogInterface
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
@@ -16,13 +21,15 @@ import com.nurshuvo.translateme.R
 import com.nurshuvo.translateme.data.repository.TranslationRepository
 import com.nurshuvo.translateme.database.entity.TranslationFavorites
 import com.nurshuvo.translateme.database.entity.TranslationHistory
-import com.nurshuvo.translateme.ui.adapter.TranslationListAdapter
 import com.nurshuvo.translateme.ui.adapter.ListItemModel
+import com.nurshuvo.translateme.ui.adapter.TranslationListAdapter
 import com.nurshuvo.translateme.ui.adapter.countOfSelectionLiveData
 import com.nurshuvo.translateme.ui.adapter.onClickedHistoryItem
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
+
 
 // can be reused for history or favourite page
 @AndroidEntryPoint
@@ -69,7 +76,58 @@ class TranslationListActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val findMenuItems = menuInflater
         findMenuItems.inflate(R.menu.history_menu, menu)
+
+        // customize search view
+        val searchItem = menu?.findItem(R.id.actionSearch)
+        val searchView = searchItem?.actionView as SearchView
+        customizeSearchView(searchView)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                if (newText.isNotEmpty()) filter(newText)
+                else {
+                    lifecycleScope.launch {
+                        updateAdapterWithDB()
+                    }
+                }
+                return false
+            }
+        })
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun customizeSearchView(searchView: SearchView) {
+        searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text).apply {
+            setTextColor(resources.getColor(R.color.white))
+            setHintTextColor(resources.getColor(R.color.white))
+        }
+        val searchClose =
+            searchView.findViewById<View>(androidx.appcompat.R.id.search_close_btn) as ImageView
+        ImageViewCompat.setImageTintList(searchClose, ColorStateList.valueOf(ContextCompat.getColor(this, R.color.white)))
+    }
+    private fun filter(text: String) {
+        val filteredlist : MutableList<ListItemModel> = mutableListOf()
+
+        // running a for loop to compare elements.
+        for (item in listItemOrFavoriteModelList) {
+            // checking if the entered string matched with any item of our recycler view.
+            if (item.fromText.toLowerCase().contains(text.lowercase(Locale.getDefault()))
+                || item.translatedText.toLowerCase().contains(text.lowercase(Locale.getDefault()))) {
+                item.isSelected = false
+                filteredlist.add(item)
+            }
+        }
+        if (filteredlist.isEmpty()) {
+            Toast.makeText(this, "No Data Found..", Toast.LENGTH_SHORT).show()
+        } else {
+            val adapter = TranslationListAdapter(filteredlist, 0)
+            (findViewById<View>(R.id.history_recycler_view) as RecyclerView).adapter =
+                adapter
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
